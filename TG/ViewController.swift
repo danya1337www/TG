77af4d9ccce8ff47090c09c0e50a8ee0817411dd
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-08-16 23:13:20 +1000

    Initial Commit

---
+++ TG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TG
+//
+//  Created by Danil Chekantsev on 16/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
